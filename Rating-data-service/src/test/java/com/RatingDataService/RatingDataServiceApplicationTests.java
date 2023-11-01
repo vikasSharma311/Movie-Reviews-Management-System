ceeb469f6593ced2947cd0f37dcf2108af5aaf7a
vikas sharma <vikasjavadev311@gmail.com>
2023-11-02 00:00:03 +0530

    Init 3 projects movieCatlog,RatingData,MovieService

---
+++ Rating-data-service/src/test/java/com/RatingDataService/RatingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RatingDataService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
