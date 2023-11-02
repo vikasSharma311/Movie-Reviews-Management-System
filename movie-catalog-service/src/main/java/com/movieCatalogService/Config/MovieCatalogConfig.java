dd8016280f04ed537135ad135308cee711c7d543
vikas sharma <vikasjavadev311@gmail.com>
2023-11-03 01:29:11 +0530

    Alternative way Webclient to do Microservices

--- movie-catalog-service/src/main/java/com/movieCatalogService/Config/MovieCatalogConfig.java
+++ movie-catalog-service/src/main/java/com/movieCatalogService/Config/MovieCatalogConfig.java
@@ -3,11 +3,17 @@ package com.movieCatalogService.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class MovieCatalogConfig {
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+
+    }
+    @Bean
+    public WebClient.Builder getWebclientBuilder(){
+        return WebClient.builder() ;
     }
 }
--- movie-catalog-service/src/main/java/com/movieCatalogService/Controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/movieCatalogService/Controller/MovieCatalogController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -19,15 +20,25 @@ import java.util.stream.Collectors;
 public class MovieCatalogController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder builder;
     @RequestMapping("/catalog/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId){
         List<Rating> ratings= Arrays.asList(
                 new Rating("1234",4),
                 new Rating("56789",5)
         );
+
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:9091/movies/"+rating.getMovieId(), Movie.class);
-           return  CatalogItem.builder().name(movie.getName()).desc("Description").rating(rating.getRating()).build();
+//            Movie movie = restTemplate.getForObject("http://localhost:9091/movies/"+rating.getMovieId(), Movie.class);
+
+            //Alternative way Webclient:Reactive Programming:asynchronous
+            Movie movie = builder.build().get()
+                    .uri("http://localhost:9091/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+            return  CatalogItem.builder().name(movie.getName()).desc("Description").rating(rating.getRating()).build();
         }).collect(Collectors.toList());
 
 
